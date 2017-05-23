338f497b66227e3bdc2c05fd542127ddae6e5c18
Jesse Paris <Jesse.Paris@qlik.com>
2017-05-23 13:20:46 -0700

    first commit. not finished.

---
+++ SalesforceReportsConnector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SalesforceReportsConnector
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			if (args != null && args.Length >= 2)
+			{
+				new QvEventLogServer().Run(args[0], args[1]);
+			}
+		}
+	}
+}
---
+++ SalesforceReportsConnector/QV/QvEventLogConnection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using QlikView.Qvx.QvxLibrary;
+
+namespace introConnector.QV
+{
+	internal class QvEventLogConnection : QvxConnection
+	{
+		public override void Init()
+		{
+			QvxLog.SetLogLevels(true, true);
+
+			QvxLog.Log(QvxLogFacility.Application, QvxLogSeverity.Notice, "Init()");
+
+			var eventLogFields = new QvxField[]
+				{
+					new QvxField("Title", QvxFieldType.QVX_TEXT, QvxNullRepresentation.QVX_NULL_FLAG_SUPPRESS_DATA, FieldAttrType.ASCII),
+					new QvxField("Message", QvxFieldType.QVX_TEXT, QvxNullRepresentation.QVX_NULL_FLAG_SUPPRESS_DATA, FieldAttrType.ASCII)
+				};
+
+			MTables = new List<QvxTable>
+				{
+					new QvxTable
+						{
+							TableName = "DummyData",
+							GetRows = GetApplicationEvents,
+							Fields = eventLogFields
+						}
+				};
+		}
+
+		private IEnumerable<QvxDataRow> GetApplicationEvents()
+		{
+			QvxLog.Log(QvxLogFacility.Application, QvxLogSeverity.Notice, "GetApplicationEvents()");
+
+			for (int i = 0; i < 20; i++)
+			{
+				var row = new QvxDataRow();
+				var table = FindTable("DummyData", MTables);
+				row[table.Fields[0]] = "SomeTitle " + i;
+				row[table.Fields[1]] = i + " - This is my message. - " + i;
+				yield return row;
+			}
+		}
+
+		public override QvxDataTable ExtractQuery(string query, List<QvxTable> qvxTables)
+		{
+			/* Make sure to remove your quotesuffix, quoteprefix, 
+             * quotesuffixfordoublequotes, quoteprefixfordoublequotes
+             * as defined in selectdialog.js somewhere around here.
+             * 
+             * In this example it is an escaped double quote that is
+             * the quoteprefix/suffix
+             */
+			query = Regex.Replace(query, "\\\"", "");
+
+			return base.ExtractQuery(query, qvxTables);
+		}
+	}
+}
