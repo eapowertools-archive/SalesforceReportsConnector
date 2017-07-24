6f3206dffcedb5c600c6cf5c8c4c041b29f2ff36
Jesse Paris <jparis@gmail.com>
2017-07-24 20:16:59 +0000

    releasing first installer

--- SalesforceReportsConnector/Properties/AssemblyInfo.cs
+++ SalesforceReportsConnector/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SalesforceReportsConnector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Salesforce Reports QVX Connector.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("EA Powertools")]
 [assembly: AssemblyProduct("SalesforceReportsConnector")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.95.0.0")]
+[assembly: AssemblyFileVersion("0.95.0.0")]
